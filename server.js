115699bd225cb8d71905d1c293d04e560f4a6763
Yuichi Sugiyama <ozwald176@gmail.com>
2023-09-17 10:51:13 +0900

    update reserve meeting confirm handler

--- server.js
+++ server.js
@@ -18,87 +18,118 @@ const ZoomClientSecret = process.env.ZOOM_CLIENT_SECRET
 
 const app = express();
 
-// routing
+// Routing
 app.post('/linewebhook', line.middleware(config), (req, res) => {
     Promise
         .all(req.body.events.map(handleEvent))
         .then((result) => res.json(result));
 });
 
+// Event Handler
 async function handleEvent(event) {
     console.log(`event: ${JSON.stringify(event)}`)
 
-    if (event.type === 'message' && event.message.type === 'text' && isZoom(event.message.text)) {
-        try {
-            // Issue Zoom token
-            const token = await issueZoomToken()
-            // Create a meeting url
-            const meetingUrl = await createZoomMeeting(token)
-            // Send Reply message
-            return client.replyMessage(event.replyToken, [
-                {
-                    type: 'text',
-                    text: "わかったよ"
-                },
-                {
-                    type: 'text',
-                    text: meetingUrl
-                }
-            ]);
-        } catch (error) {
-            console.error(error);
+    if (event.type === 'message') {
+        const messageType = event.message.type
+        const text = event.message.text
+        if (messageType === 'text' && isZoom(text)) {
+            try {
+                // Issue Zoom token
+                const token = await issueZoomToken()
+                // Create a meeting url
+                const meetingUrl = await createZoomMeeting(token)
+                // Send Reply message
+                return client.replyMessage(event.replyToken, [
+                    {
+                        type: 'text',
+                        text: "わかったよ"
+                    },
+                    {
+                        type: 'text',
+                        text: meetingUrl
+                    }
+                ]);
+            } catch (error) {
+                console.error(error);
+            }
+        } else if (messageType === 'text' && isReserve(text)) {
+            try {
+                // Send datetimepicker
+                // API Reference: https://developers.line.biz/ja/reference/messaging-api/#buttons
+                return client.replyMessage(event.replyToken, {
+                    "type": "template",
+                    "altText": "This is a datetime_picker for zoom meeting",
+                    "template": {
+                        "type": "buttons",
+                        "title": "zoomミーティングの予約",
+                        "text": "日時を選んでね",
+                        "actions": [
+                            {
+                                "type": "datetimepicker",
+                                "label": "日時を選択",
+                                "data": "action=reserve-zoom-meeting",
+                                "mode": "datetime",
+                            }
+                        ]
+                    }
+                });
+            } catch (error) {
+                console.error(error);
+            }
+        } else {
+            return Promise.resolve(null);
         }
-    } else if (event.type === 'message' && event.message.type === 'text' && isReserve(event.message.text)) {
-        try {
-            // Send datetimepicker
-            // API Reference: https://developers.line.biz/ja/reference/messaging-api/#buttons
-            return client.replyMessage(event.replyToken, {
-                "type": "template",
-                "altText": "This is a datetime_picker for zoom meeting",
-                "template": {
-                    "type": "buttons",
-                    "title": "zoomミーティングの予約",
-                    "text": "日時を選んでね",
-                    "actions": [
-                        {
-                            "type": "datetimepicker",
-                            "label": "日時を選択",
-                            "data": "reserve-zoom-meeting",
-                            "mode": "datetime",
-                        }
-                    ]
-                }
-            });
-        } catch (error) {
-            console.error(error);
-        }
-    } else if (event.type === 'postback' && event.postback.data === 'reserve-zoom-meeting') {
-        const datetime = formatDate(event.postback.params.datetime)
-        try {
-            // Send Reply message
-            // API Reference: https://developers.line.biz/ja/reference/messaging-api/#confirm
-            return client.replyMessage(event.replyToken, {
-                "type": "template",
-                "altText": "this is a confirm template for zoom meeting reservation",
-                "template": {
-                    "type": "confirm",
-                    "text": `${datetime} で問題ないかな?`,
-                    "actions": [
-                        {
-                            "type": "postback",
-                            "label": "はい",
-                            "data": `action=reserve-confirm-yes&datetime=${event.postback.data}`,
-                        },
-                        {
-                            "type": "postback",
-                            "label": "いいえ",
-                            "data": "action=reserve-confirm-no",
-                        }
-                    ]
-                }
-            })
-        } catch (error) {
-            console.error(error);
+    } else if (event.type === 'postback') {
+        const data = parseData(event.postback.data)
+        if (data.action === 'reserve-zoom-meeting') {
+            const datetime = formatDate(event.postback.params.datetime)
+            try {
+                // Send Reply message
+                // API Reference: https://developers.line.biz/ja/reference/messaging-api/#confirm
+                return client.replyMessage(event.replyToken, {
+                    "type": "template",
+                    "altText": "this is a confirm template for zoom meeting reservation",
+                    "template": {
+                        "type": "confirm",
+                        "text": `${datetime} で問題ないかな?`,
+                        "actions": [
+                            {
+                                "type": "postback",
+                                "label": "はい",
+                                "data": `action=reserve-confirm-yes&datetime=${event.postback.data}`,
+                            },
+                            {
+                                "type": "postback",
+                                "label": "いいえ",
+                                "data": "action=reserve-confirm-no",
+                            }
+                        ]
+                    }
+                })
+            } catch (error) {
+                console.error(error);
+            }
+        } else if (data.action === 'reserve-confirm-yes') {
+            // TODO: impl
+            try {
+                return client.replyMessage(event.replyToken, {
+                    type: 'text',
+                    text: "以下の日時でミーティングを予約したよ"
+                });
+            } catch (error) {
+                console.error(error);
+            }
+        } else if (data.action === 'reserve-confirm-no') {
+            try {
+                return client.replyMessage(event.replyToken, {
+                        type: 'text',
+                        text: "ミーティングの予約を中止したよ"
+                    });
+            } catch (error) {
+                console.error(error);
+            }
+        } else {
+            return Promise.resolve(null);
         }
     } else {
         return Promise.resolve(null);
@@ -180,6 +211,16 @@ function formatDate(inputDate) {
     }
 
     return `${year}年${month}月${day}日 ${hours}時${minutes}分`;
+}
+
+function parseData(inputString) {
+    const parts = inputString.split('&');
+    const o = {};
+    for (const part of parts) {
+        const [key, value] = part.split('=');
+        o[key] = value
+    }
+    return o
 }
 
 function isZoom(text) {
