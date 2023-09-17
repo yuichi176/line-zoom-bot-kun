2c928788929bc6534744e0b8faa5d321c4a0c130
Yuichi Sugiyama <ozwald176@gmail.com>
2023-09-17 20:31:52 +0900

    update datetime format

--- server.js
+++ server.js
@@ -91,7 +91,7 @@ async function handleEvent(event) {
                     "altText": "this is a confirm template for zoom meeting reservation",
                     "template": {
                         "type": "confirm",
-                        "text": `${datetime}で問題ないかな?`,
+                        "text": `「${datetime}」\nで問題ないかな?`,
                         "actions": [
                             {
                                 "type": "postback",
@@ -209,15 +209,21 @@ function formatDate(inputDate) {
     const date = new Date(inputDate);
 
     const year = date.getFullYear();
-    const month = date.getMonth() + 1; // 月は0から始まるため+1
-    const day = date.getDate();
+    let month = date.getMonth() + 1; // 月は0から始まるため+1
+    let day = date.getDate();
     const hours = date.getHours();
     let minutes = date.getMinutes();
+    if (month.toString().length === 1) {
+        month = `0` + month
+    }
+    if (day.toString().length === 1) {
+        day = `0` + day
+    }
     if (minutes.toString().length === 1) {
         minutes = `0` + minutes
     }
 
-    return `${year}年${month}月${day}日 ${hours}時${minutes}分`;
+    return `${year}/${month}/${day} ${hours}:${minutes}`;
 }
 
 function parseData(inputString) {
